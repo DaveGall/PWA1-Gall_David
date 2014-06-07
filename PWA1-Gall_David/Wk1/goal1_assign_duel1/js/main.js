f6cd5743baa0c742bd4fd868aa8f101fcf06c87b
DaveGall <dlaker74@fullsail.edu>
2014-06-06 19:06:40 -0700

    Creating the equations for the damage.

--- PWA1-Gall_David/Wk1/goal1_assign_duel1/js/main.js
+++ PWA1-Gall_David/Wk1/goal1_assign_duel1/js/main.js
@@ -15,9 +15,19 @@
 
     function fight(){
        console.log("In the fight function");
+
         alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
        for(var i = 0; i < 10; i++){
-          console.log(i);
+
+           var minDamage1 = player1Damage * .5;
+           var minDamage2 = player2Damage * .5;
+
+           var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+           var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+           playerOneHealth-=f1;
+           playerTwoHealth-=f2;
+
        };
     };
 
