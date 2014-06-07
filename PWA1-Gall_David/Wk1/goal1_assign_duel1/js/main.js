58db031071516ddcdd14175e32ab73dcafd86e65
DaveGall <dlaker74@fullsail.edu>
2014-06-06 18:38:36 -0700

    Setting up the variables for dual part1. Player, players health, damage.

--- PWA1-Gall_David/Wk1/goal1_assign_duel1/js/main.js
+++ PWA1-Gall_David/Wk1/goal1_assign_duel1/js/main.js
@@ -1,2 +1,14 @@
 //David Gall, 06/06/2014, Dual Part1
 
+(function(){
+    //Player Names
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+    //Player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+    //Player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
+
+})();
