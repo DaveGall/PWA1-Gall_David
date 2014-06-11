43d7bb3be894484c1ae47ac1a11bdd6ef162ee83
DaveGall <dlaker74@fullsail.edu>
2014-06-11 15:27:14 -0700

    I have put some if statements to display different results for different inputs.

--- PWA1-Gall_David/Wk2/goal4_assign_guessing_game/js/main.js
+++ PWA1-Gall_David/Wk2/goal4_assign_guessing_game/js/main.js
@@ -18,14 +18,21 @@
         output: document.querySelector("#output"),
         button: document.querySelector("button")
     };
-
+    //dom.button.onkeydown(validateInput());
     var validateInput = function(){
         playersGuess = parseInt(dom.input.value);
 
         if(isNaN(playersGuess)){//Put in valid information
-            document.write("Please enter a number!!");
+            console.log("Please enter a number!!");
 
+        }if(playersGuess < randomNum){
+            console.log("Your guess is too low!");
+        }if(playersGuess > randomNum){
+            console.log("Your guess is too high!");
+        }else{
+            console.log("You guessed my number");
         }
     };
 
 })();
+
