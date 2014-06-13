fdd1715df324cf8ee3da0bb7b025a6815869a70e
DaveGall <dlaker74@fullsail.edu>
2014-06-12 17:31:59 -0700

    I am submitting this commit before I finish with it because I need to work on reading for my quiz. I will pick up the pieces and try it when I finish my reading and quiz. I am stuck at the moment anyway.

--- PWA1-Gall_David/Wk2/goal4_assign_guessing_game/js/main.js
+++ PWA1-Gall_David/Wk2/goal4_assign_guessing_game/js/main.js
@@ -12,13 +12,15 @@
 
     var randomNum = Math.floor(Math.random() * 10) + 1;//This line gets a random number between 1 and 10 for each round.
     console.log(randomNum);//This displays the results of the variable randomNum to make sure I am getting a random number.
-
+    var guess = 0;
     var dom = {//Object
         input: document.querySelector("#input"),//references the input field of the HTML.
         output: document.querySelector("#output"),//References the output of the HTML
         button: document.querySelector("button")//References the button of the HTML.
     };
-    dom.button.onclick = function() {//Beginning of the function that tells the code to press the guess button to display results when clicked.
+
+        //i = dom.button.onclick;
+      dom.button.onclick = function() {//Beginning of the function that tells the code to press the guess button to display results when clicked.
 
         var validateInput = function () {//Beginning of the function that will run the users guess
             var playersGuess = parseInt(dom.input.value);//Creates a variable for the users guess by pulling the value from the input field.
@@ -40,17 +42,30 @@
             }
             return result;//This will return the result of the correct conditional.
 
+
         };
-        alert(validateInput(randomNum));
-        document.getElementById("output");
+
+          var guessCheck = validateInput();
+          for(var i = 0; i < 3; i++){
+              if(guessCheck !== "You guessed my number!" ){
+                  guess++;
+                  alert('Take another shot');
+              }else{
+                  alert(guessCheck);
+                  break;
+              }
+          }
+          //alert(validateInput(randomNum));
+        //document.getElementById("output");
 
 
         console.log(validateInput(randomNum));//This line runs the random number through the validateInput function.
 
      };
-    for(var i = 0, j = 3; i < j; i++){
 
-    }
+
+
+
 
 
     //dom.button.onclick(randomNum);
