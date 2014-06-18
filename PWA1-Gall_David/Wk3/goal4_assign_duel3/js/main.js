4f705ab15888d99d3b67e73745aceb88aefc7473
DaveGall <dlaker74@fullsail.edu>
2014-06-17 21:33:45 -0700

    I have placed the button call function in the proper place, however now I cannot tally the first round. It starts subtracting the score after the first round.

--- PWA1-Gall_David/Wk3/goal4_assign_duel3/js/main.js
+++ PWA1-Gall_David/Wk3/goal4_assign_duel3/js/main.js
@@ -39,41 +39,42 @@
     fighter2_txt.innerHTML = fighters[1].name+":"+fighters[1].health;
 
     var round = 0;//Start on round zero so after the first fight call it says you are done with round one.
+    function onClick(){
+        fight()
+    };
 
     function fight() {//The fight function
+        round_txt.innerHTML = "Click FIGHT BUTTON to Start!";
         fighter1_txt.innerHTML = fighters[0].name + ":" + fighters[0].health;
         fighter2_txt.innerHTML = fighters[1].name + ":" + fighters[1].health;
         //alert(fighter1[0]+":"+fighter1[2]+" *START* "+fighter2[0]+":"+fighter2[2]);//The beginning alert to start the fight.
         //alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);//The beginning alert to start the fight.
         var minDamage1 = fighters[0].damage * .5;//Variable equation for the damage to each player.
         var minDamage2 = fighters[1].damage * .5;//Variable equation for the damage to each player.
-        /*var f1 = Math.floor(Math.random() * (fighters[0].damage - minDamage1) + minDamage1);//Variable to determine a random number to subtract from each player at the end of each round.*/
-        var f1 = Math.floor(Math.random() * fighters[0].damage + fighters[0].damage * .5);
+        var f1 = Math.floor(Math.random() * (fighters[0].damage - minDamage1) + minDamage1);//Variable to determine a random number to subtract from each player at the end of each round.
         var f2 = Math.floor(Math.random() * (fighters[1].damage - minDamage2) + minDamage2);//Variable to determine a random number to subtract from each player at the end of each round.
 
 
         fighters[0].health -= f1;//This subtracts the players health by the amount of damage from the prior variables.
         fighters[1].health -= f2;//This subtracts the players health by the amount of damage from the prior variables
+        var results = winnerCheck();//This is a variable that will check to see if there is a winner after each round.
 
+console.log(results);
 
-        /*   for(var i = 0; i < 10; i++){//Takes the variable i and runs through the loop as long as its below 10.
-
-         console.log(fighter1[0]+":"+fighter1[2]+" "+fighter2[0]+":"+fighter2[2]);
-         //console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);//This was a print out to see if the code was giving us the results we were looking for.
-
-         var results = winnerCheck();//This is a variable that will check to see if there is a winner after each round.
-         console.log(results);//Displays the results to the console.
 
          if(results === "no winner"){//If this condition is true then it will run the next line
          round++;//This will progress the fight another round
-         alert(fighter1[0]+":"+fighter1[2]+" *ROUND "+round+" OVER* "+fighter2[0]+":"+fighter2[2]);//Displays the results after each round.
+         //alert(fighter1[0]+":"+fighter1[2]+" *ROUND "+round+" OVER* "+fighter2[0]+":"+fighter2[2]);//Displays the results after each round.
+             round_txt.innerHTML = "Round "+ round+" complete";
+             console.log("Round: "+ round);
 
          }else{//This else statement will display the results once the if statement is false and there is a winner.
          alert(results);//Will display the results
-         break;//stops the code from running once there is a winner.
+             console.log(results);
+
          };
-         };
-         };*/
+
+
 
         function winnerCheck() {//This code is to determine the parameters of how a winner is crowned.
             console.log("in winnerCheck FN");
@@ -82,14 +83,14 @@
             if (fighters[0].health < 1 && fighters[1].health < 1) {//If this condition is true then the following result will display.
                 result = "You both die";//Display if above code is true
             } else if (fighters[0].health < 1) {//If this condition is true then the code below will display
-                result = fighters[1].name + " WINS!!!";//Code to display if the results of the above condition are true.
+                result = fighter2_txt.innerHTML
+                    //fighters[1].name + " WINS!!!";//Code to display if the results of the above condition are true.
             } else if (fighters[1].health < 1) {//Condition to check to see if all the other conditions returned false then this will display.
                 result = fighters[0].name + " WINS!!!";//Code that displays if the above statement is true.
             }
             return result;//Returns a value for the results of the function.
         };
     }
-        //console.log("program starts here");
-        fight();//Runs the function for fight.
+        
 
 })();//End of program.
