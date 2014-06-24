fcfe92eef1eb8a3741ff4eb8083daeed50a1174e
DaveGall <dlaker74@fullsail.edu>
2014-06-24 12:11:39 -0700

    Set the variables to get the rows/column from the HTML file so that I could randomly select one. I have also created the formula to randomly select one.

--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -6,7 +6,30 @@
  */
 
 var names = ["Dave", "Maria", "Bill", "Jill", "Larry"];
-
+var people = [];
+var row1 = document.getElementById("r1c1");
+var row2 = document.getElementById("r2c1");
+var row3 = document.getElementById("r3c1");
+var rows = [row1, row2, row3];
 var randomName = names[Math.floor(Math.random() * names.length)];
-console.log(randomName);
+var randomRows = rows[Math.floor(Math.random() * rows.length)];
 
+
+
+console.log("These are the random rows: ",randomRows);
+
+
+
+
+
+
+
+
+
+/*for(var i = 0; i <= 3; i++){
+
+    people.push(names);
+    console.log(people);
+
+    }
+*/
