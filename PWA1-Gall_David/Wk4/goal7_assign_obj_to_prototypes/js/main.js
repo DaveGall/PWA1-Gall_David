db010f81c67b70ae082e97f686611b8a66d277d3
DaveGall <dlaker74@fullsail.edu>
2014-06-24 11:20:41 -0700

    I have set up my array for the names and I have created the formula for randomly picking a name from my names array.

--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -1,7 +1,12 @@
 /**
  * David Gall
  * Class: PWA1
- * Goal: Goal7
+ * Goal: Goal7 week 4 final project
  * 06/24/14
  */
+
+var names = ["Dave", "Maria", "Bill", "Jill", "Larry"];
+
+var randomName = names[Math.floor(Math.random() * names.length)];
+console.log(randomName);
 
--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -1,6 +1,6 @@
 /**
  * David Gall
  * Class: PWA1
- * Goal: Goal7
+ * Goal: Goal7 week 4 final project
  * 06/24/14
  */
