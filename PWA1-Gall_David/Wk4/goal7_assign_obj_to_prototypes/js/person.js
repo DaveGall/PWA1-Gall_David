d1c7a91e3279cf5f793aa12eac70ab306c6811ef
DaveGall <dlaker74@fullsail.edu>
2014-06-25 16:26:56 -0700

    Created the Person object and its values. I had my math.random in the wrong place so that has been corrected. I added the getElementbyId to the right spot as I had it wrong prior to watching the video of the screencast.

--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -8,7 +8,21 @@
 (function(){
 
 
+window.Person=Person;
 
+Person.jobs = ["Vet", "Chef", "Producer", "Actress", "Programmer"];
+Person.actions = ["Cooking", "Running", "Singing", "Drinking"];
+
+function Person(name, row){
+    console.log("New Peeps: ", name);
+
+    this.name = name;
+    this.action = Person.action[Math.floor(Math.random() * Person.action.length)];
+    this.job = Person.job[Math.floor(Math.random() * Person.job.length)];
+    this.row = row;
+    var id = document.getElementById("r"+this.row+"c3");
+    id.innerHTML = this.action;
+}
 
 
 
