9d769ef3bf2ef9cf1bebffec3f88561aed58a7f8
DaveGall <dlaker74@fullsail.edu>
2014-06-25 20:27:19 -0700

    Finished commenting both the main and person js files.

--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/main.js
@@ -11,31 +11,32 @@
     var numPeeps = 3;//variable that sets the number of people to display at 3.
     var people = [];//Array to store the results of the for loop below.
     var names = ["Dave", "Maria", "Bill", "Jill", "Larry"];//name array that the for loop will pull from.
-    var interval;
+    var interval;//variable that will be used set and terminate the interval timing.
 
 
     for (var i = 0; i < numPeeps; i++) {//Cycles through and chooses only three names from my names list.
-        var newPeeps = Math.floor(Math.random() * names.length);
-        var person = new Person(names[newPeeps], i + 1);
+        var newPeeps = Math.floor(Math.random() * names.length);//Randomly grabs names out of the names array.
+        var person = new Person(names[newPeeps], i + 1);//This will add a person to the People object.
 
-        populateHTML(person.name, "r" + (i + 1) + "c1");
-        populateHTML(person.job, "r" + (i + 1) + "c2");
+        populateHTML(person.name, "r" + (i + 1) + "c1");//This will update the HTML with a random name in column one.
+        populateHTML(person.job, "r" + (i + 1) + "c2");//This will update the HTML with a random job in column two.
 
-        people.push(person);
-        names.splice(newPeeps, 1);
+        people.push(person);//This will push the name taken from the names array and place it into the people array.
+        names.splice(newPeeps, 1);//This will remove the name from the names array.
 
     }
-    console.log("These are the people: ", people+person);
-    clearInterval(interval);
-    interval = setInterval(runUpdate, 10000 / 30);
 
-    function populateHTML(date, field) {
-        var id = document.getElementById(field);
-        id.innerHTML = date;
+    clearInterval(interval);//This clears the interval.
+    interval = setInterval(runUpdate, 10000 / 30);//This line sets up the interval at which the function will update.
+
+    function populateHTML(date, field) {//This function that will run when called by populateHTML
+        var id = document.getElementById(field);//This grabs the second parameter passed through the function from the HTML document.
+        id.innerHTML = date;//This line writes the new information to the HTML
+
     }
 
     function runUpdate() {
-        people.forEach(function (element) {
+        people.forEach(function (element) {//This function will run when called by the runUpdate keywords.
 
             element.update();
         })
--- PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
+++ PWA1-Gall_David/Wk4/goal7_assign_obj_to_prototypes/js/person.js
@@ -8,29 +8,29 @@
 
 (function() {
 console.log("This is person.js");
-    window.Person = Person;
+    window.Person = Person;//Makes the Person object a global variable that can be used by the main.js file also.
 
-    Person.jobs = ["Vet", "Chef", "Producer", "Actress", "Programmer"];
-    Person.actions = ["Cooking", "Running", "Singing", "Drinking"];
+    Person.jobs = ["Vet", "Chef", "Producer", "Actress", "Programmer"];//Declares a variable array that is added to the Person object as jobs.
+    Person.actions = ["Cooking", "Running", "Singing", "Drinking"];//Declares a variable array that is added to the Person object as actions.
 
-    function Person(name, row) {
+    function Person(name, row) {//Advanced object called Person that can be added to and used to handle a lot of information
         console.log("New Peeps: ", name);
 
-        this.name = name;
-        this.action = Person.actions[Math.floor(Math.random() * Person.actions.length)];
-        this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];
-        this.row = row;
-        var id = document.getElementById("r" + this.row + "c3");
-        id.innerHTML = this.action;
+        this.name = name;//Accesses the name to the Person object and gives it a value of name which is passed through as the second argument.
+        this.action = Person.actions[Math.floor(Math.random() * Person.actions.length)];//Randomly grabs actions form the actions array
+        this.job = Person.jobs[Math.floor(Math.random() * Person.jobs.length)];//Randomly grabs jobs from the jobs array.
+        this.row = row;//This sets row to the second argument run through the object.
+        var id = document.getElementById("r" + this.row + "c3");//This grabs the row 1 from the HTML.
+        id.innerHTML = this.action;//Writes the results to the HTML page.
     }
 
 
-    Person.prototype.update = function () {
-        if (Math.floor(Math.random()) < .01) {
-            var i = Math.floor(Math.random() * Person.actions.length);
-            this.action = Person.actions[i];
-            var id = document.getElementById("r" + this.row + "c3");
-            id.innerHTML = this.action;
+    Person.prototype.update = function () {//This function runs the udater that will change the actions in column 3.
+        if (Math.floor(Math.random()) < .01) {//Keeps running as long as Math.floor is less than .01.
+            var i = Math.floor(Math.random() * Person.actions.length);//Sets the variable i to the random actions.
+            this.action = Person.actions[i];//This runs the variable i through the Persons.action object.
+            var id = document.getElementById("r" + this.row + "c3");//Grabs the random row from the HTML.
+            id.innerHTML = this.action;//Writes the random action to the random row in column 3.
 
         }
     };
